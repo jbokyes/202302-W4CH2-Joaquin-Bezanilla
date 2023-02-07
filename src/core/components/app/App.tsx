f6f5f733e8dd0dbbfc627fc99659948d6f7bead5
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-07 18:49:38 +0100

    added header.

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { Header } from "../header/header";
+
+function App() {
+  return <Header></Header>;
+}
+
+export default App;
---
+++ src/core/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <header className="App-header">
+      <h1>Formulario de tres pasos</h1>
+    </header>
+  );
+}
