14060ff3cba54bb0a633aa717ed4be08225b9174
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-07 19:56:58 +0100

    add home tsx, with form as a component + its own css

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
+import HomePage from "../../../features/home/page/home";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <HomePage></HomePage>
       <Footer></Footer>
     </>
   );
