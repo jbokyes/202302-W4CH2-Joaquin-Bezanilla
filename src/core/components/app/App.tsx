597f7a30dc473b6a1db4987dfb4a6901a3e4aa73
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-07 21:50:19 +0100

    created form2 and did something with form1

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -4,6 +4,16 @@ import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
 import HomePage from "../../../features/home/page/home";
 
+export type FormOption = {
+  label: string;
+  path: string;
+};
+
+const formOptions: FormOption[] = [
+  { label: "Inicio", path: "/home" },
+  { label: "Inicio2", path: "/tasks" },
+  { label: "Inicio3", path: "/about.html" }
+];
 function App() {
   return (
     <>
---
+++ src/features/home/components/form2/form2.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Form2() {
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" name="username" />
+      </label>
+      <label>
+        Password:
+        <input type="text" name="password" />
+      </label>
+      <label>
+        Repeat password:
+        <input type="text" name="repeatpassword" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
